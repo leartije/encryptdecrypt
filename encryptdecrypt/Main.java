86c8e8447217b6eed6b74c56e5da7336b132c7c5
leartije <reciska@gmail.com>
2022-05-13 12:11:01 +0200

    Stage 4 complete

--- encryptdecrypt/Main.java
+++ encryptdecrypt/Main.java
@@ -4,9 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String s = Encryption.scanner.nextLine();
-        String msg = Encryption.scanner.nextLine();
-        int n = Encryption.scanner.nextInt();
+        String s = "enc";
+        String msg = "";
+        int n = 0;
+
+        for (int i = 0; i < args.length; i++) {
+            if ("-mode".equals(args[i])) {
+                s = args[i + 1];
+            }
+            if ("-key".equals(args[i])) {
+                n = Integer.parseInt(args[i + 1]);
+            }
+            if ("-data".equals(args[i])) {
+                msg = args[i + 1];
+            }
+        }
+
 
         if ("enc".equalsIgnoreCase(s)) {
             System.out.println(new Encryption().execute(msg, n));
